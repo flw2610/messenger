3000a49b46baa4723f9114d0c6973c34649ab1c9
flw2610 <ai.flow@icloud.com>
2025-12-16 21:02:57 +0100

    added most

--- src/logout.php
+++ src/logout.php
@@ -6,12 +6,23 @@ session_unset();
 <html>
   <head>
     <link rel="stylesheet" href="stylesheet.css" />
+    <!-- CSS-Framework von Bootstrap -->
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet'
+  crossorigin='anonymous'>
   </head>
 
   <body class="inOut">
+    
     <img src="images/logout.png" alt="Logging-Out-Logo" />
-    <h1><b>Logged out...</b></h1>
+    <div class="mb-5"></div>
+    <div class="login_register p-5">
+      <h1><b>Logged out...</b></h1>
     <p>See u!</p>
-    <a class="nav" href="./login.php">Login again</a>
+    <a  href="./login.php"><button class="btn btn-secondary">Login again</button></a>
+    </div>
+    <!-- Notwendige JavaScript-Abhängigkeiten -->
+  <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js'
+    crossorigin='anonymous'></script>
+  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js' crossorigin='anonymous'></script>
   </body>
 </html>
